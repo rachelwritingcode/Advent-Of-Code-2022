cfc9ec2ba00d2a696d1bbaae273e333ec0f911fc
rachelwritingcode <21071004+rachelwritingcode@users.noreply.github.com>
2022-12-02 23:50:41 -0500

    Add day 2 part 2

--- day2.py
+++ day2.py
@@ -2,6 +2,35 @@ X = 1 # rock
 Y = 2 # paper
 Z = 3 # scissors
 
+def get_outcome_score(player_one, player_two):
+
+    score = 0
+    if player_two == 'X':
+        if player_one == 'A':
+            score += 3
+        elif player_one == 'B':
+            score += 1
+        elif player_one == 'C':
+            score += 2
+    elif player_two == 'Y':
+        score += 3
+        if player_one == 'A':
+            score += 1
+        elif player_one == 'B':
+            score += 2
+        elif player_one == 'C':
+            score += 3
+    elif player_two == 'Z':
+        score += 6
+        if player_one == 'A':
+            score += 2
+        elif player_one == 'B':
+            score += 3
+        elif player_one == 'C':
+            score += 1
+    return score
+
+
 def get_total_score(player_one, player_two):
 
     total = 0
@@ -34,16 +63,21 @@ def get_total_score(player_one, player_two):
 
 
 def main():
-    f = open("guide.txt", "r")
+    f = open("strategy_guide.txt", "r")
     rounds = f.readlines()
     total_score = 0
+    outcome_score = 0
 
     for i in range(len(rounds)):
         p1 = rounds[i][0]
         p2 = rounds[i][2]
-        total_score += get_total_score(p1, p2)
+        total_score += get_total_score(p1, p2) # part 1
+        outcome_score += get_outcome_score(p1, p2) # part 2
 
-    print(total_score)
+    print(total_score) 
+    print(outcome_score)
+
+
 
 if __name__ == '__main__':
     main()
