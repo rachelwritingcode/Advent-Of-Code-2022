fa257513664d04711dbe8f23545b5d310b920257
rachelwritingcode <21071004+rachelwritingcode@users.noreply.github.com>
2022-12-08 20:46:38 -0500

    Add day 8 p2

--- day8.py
+++ day8.py
@@ -1,6 +1,7 @@
 def main():
     
     tree_counter = 0
+    scenic_score = 0
     trees = []
     with open("./input/day8-input.txt") as f:
         for line in f:
@@ -9,6 +10,7 @@ def main():
 
     for i, row in enumerate(trees):
         for j, height in enumerate(row):
+            score = 1
             isVisible = False
             treelines = [
                 row[:j][::-1],
@@ -20,11 +22,17 @@ def main():
             for treeline in treelines:
                 for dist, h in enumerate(treeline, 1):
                     if h >= height:
+                        score *= dist
                         break
                 else:
                     isVisible = True
+                    score *= max(1, len(treeline))
+
             tree_counter += int(isVisible)
-    print(tree_counter)
+            scenic_score = max(score, scenic_score)
+
+    print(tree_counter) # Part 1
+    print(scenic_score) # Part 2
 
 
 if __name__ == '__main__':
